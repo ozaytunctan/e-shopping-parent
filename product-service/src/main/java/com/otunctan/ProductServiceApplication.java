ed54cb6cd2f32f6fbc3636ea4a841c505790d436
ozaytunctan <ozaytunctan@gmail.com>
2024-02-13 23:06:49 +0300

    feign client entegre edildi.

--- product-service/src/main/java/com/otunctan/ProductServiceApplication.java
+++ product-service/src/main/java/com/otunctan/ProductServiceApplication.java
@@ -1,11 +1,12 @@
 package com.otunctan;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
@@ -14,5 +15,18 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class ProductServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class,args);
+    }
+
+
+    @Value("${jenkis.build.number}")
+    private String buildNumber;
+    @Bean
+    public CommandLineRunner jenkinsBuildNumber(){
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                System.out.println(buildNumber);
+            }
+        };
     }
 }
